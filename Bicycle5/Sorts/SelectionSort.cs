41f3c321f5dc8ff45f5bd227bf696f24081deb6c
WurgerZax <igoridze.reap@gmail.com>
2025-01-04 15:00:53 +0200

    [feature] task3

--- Bicycle5/Sorts/SelectionSort.cs
+++ Bicycle5/Sorts/SelectionSort.cs
@@ -6,8 +6,22 @@ public class SelectionSort: ISort
 {
     public int[] Sort(int[] source)
     {
+        int[] result = (int[])source.Clone();
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            int currMinIndex = i;
+            
+            for (int j = i + 1; j < result.Length; j++)
+            {
+                if (result[j] < result[currMinIndex])
+                {
+                    currMinIndex = j;
+                }    
+            }
+            (result[i], result[currMinIndex]) = (result[currMinIndex], result[i]);
+        }
         
-        
-        return source;
+        return result;
     }   
 }
