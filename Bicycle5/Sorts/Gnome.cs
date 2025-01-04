2a9e98c61aa2914b82c08d8667a7e6d1e34aee0d
WurgerZax <igoridze.reap@gmail.com>
2025-01-04 13:19:12 +0200

    [cleanup]

--- Bicycle5/Sorts/Gnome.cs
+++ Bicycle5/Sorts/Gnome.cs
@@ -6,13 +6,11 @@ public class GnomeSort: ISort
 {
     public int[] Sort(int[] source)
     {
-        int[] result = (int[])source.Clone();
-        
-        for (int i = 1; i < result.Length;)
+        for (int i = 1; i < source.Length;)
         {
-            if (result[i] < result[i-1])
+            if (source[i] < source[i-1])
             {
-                (result[i-1], result[i]) = (result[i], result[i-1]);
+                (source[i-1], source[i]) = (source[i], source[i-1]);
                 i--; // go "left" -> check previous elements with current
                 i = i < 1 ? 1 : i; // if got to start of array -> we checked all elements from start to current with current
             }
@@ -22,6 +20,6 @@ public class GnomeSort: ISort
             }
         }
         
-        return result;
+        return source;
     }
 }
---
+++ Bicycle5/Sorts/SelectionSort.cs
@@ -0,0 +1,12 @@
+using Bicycle5.Interfaces;
+
+namespace Bicycle5.Sorts;
+
+public class SelectionSort: ISort
+{
+    public int[] Sort(int[] source)
+    {
+
+        return source;
+    }   
+}
